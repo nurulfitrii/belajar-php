6bac5ac7ef890ee582d8b8e7052c5c36caa739ed
Nurul Fitri <nurulfitri79509@gmail.com>
2022-10-24 11:33:58 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama = "Nurul Fitri";
+$umur = 20;
+
+echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+$namaAdik = "Lila";
+$umurAdik = 17;
+
+$selisihumur = $umur-$umurAdik ;
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun.<br>";
+
+
+?>
